66953fff7373613fb8144f876d66930f64b2eb72
Wojciech Mazurowski <wojci123@gmail.com>
2023-03-14 16:01:12 +0100

    page hero init

--- emakreting-bullel/src/components/pageHero/pageHero.tsx
+++ emakreting-bullel/src/components/pageHero/pageHero.tsx
@@ -1,6 +1,18 @@
-
-
+import React from "react";
+import heroVideo from "../../assets/pageHeroVideo.mp4"
+import ReactPlayer from "react-player";
+import "./pageHero.css"
 export default function PageHero(){
 
-    return <div>PAGEHERO</div>
+    return <div className={"pageHeroVideo"}>
+        <ReactPlayer
+            className={"react-player"}
+            url={heroVideo}
+            playing
+            loop
+            muted
+            width="100%"
+            height="100%"
+        />
+    </div>
 }
