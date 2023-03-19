23cd2987e562abb7306335298cfec2e648290db2
mateusz-stelmasiak <46268673+mateusz-stelmasiak@users.noreply.github.com>
2023-03-19 17:10:42 +0100

    Contact form and so on and so on

--- emakreting-bullel/src/App.tsx
+++ emakreting-bullel/src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import PageHero from "./components/pageHero/pageHero";
 import AboutSection from "./components/testSection/aboutSection";
 import {Col, Row} from "antd";
+import Contact from "./components/contactForm/Contact";
 
 function App() {
   return (
     <Row className="App">
         <Col xxl={{offset:2,span:20}} xl={{span:24, offset:0}}>
-        <PageHero/>
-        <AboutSection/>
+            <PageHero/>
+            <AboutSection/>
+            <Contact/>
         </Col>
     </Row>
   );
