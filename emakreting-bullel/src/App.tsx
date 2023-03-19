74ffc842ccd183cefaf657938d8cdb8e4c0b68a6
Wojciech-Mazurowski-digimuth <wojciech.mazurowski@digimuth.com>
2023-03-19 17:02:45 +0100

    reviews component

--- emakreting-bullel/src/App.tsx
+++ emakreting-bullel/src/App.tsx
@@ -3,16 +3,18 @@ import './App.css';
 import PageHero from "./components/pageHero/pageHero";
 import AboutSection from "./components/testSection/aboutSection";
 import {Col, Row} from "antd";
+import ReviewsSection from "./components/reviewsSection/reviewsSection";
 
 function App() {
-  return (
-    <Row className="App">
-        <Col xxl={{offset:2,span:20}} xl={{span:24, offset:0}}>
-        <PageHero/>
-        <AboutSection/>
-        </Col>
-    </Row>
-  );
+    return (
+        <Row className="App">
+            <Col xxl={{offset: 2, span: 20}} xl={{span: 24, offset: 0}}>
+                <PageHero/>
+                <AboutSection/>
+                <ReviewsSection/>
+            </Col>
+        </Row>
+    );
 }
 
 export default App;
---
+++ emakreting-bullel/src/components/reviewsSection/reviewsSection.tsx
@@ -0,0 +1,28 @@
+import "./reviewsSection.css"
+import {Col, Row} from "antd";
+
+export default function ReviewsSection() {
+
+    return <div className={"reviewsWrapper"}>
+        <div className={"reviewHeader"}>
+            REVIEWS
+            <div className={"reviewHeaderDecoration"}>
+                REVIEWS
+            </div>
+        </div>
+        <div className={"reviewsImages"}>
+            <div className={"reviewPlaceholder"}>
+
+            </div>
+            <div className={"reviewPlaceholder"}>
+
+            </div>
+            <div className={"reviewPlaceholder"}>
+
+            </div>
+            <div className={"reviewPlaceholder"}>
+
+            </div>
+        </div>
+    </div>
+}
