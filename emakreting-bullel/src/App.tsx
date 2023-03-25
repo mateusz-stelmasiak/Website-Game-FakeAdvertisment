7f39b72a4197178828ec3e966ab80f8e83b15f54
Wojciech-Mazurowski-digimuth <wojciech.mazurowski@digimuth.com>
2023-03-26 00:44:23 +0100

    more text

--- emakreting-bullel/src/App.tsx
+++ emakreting-bullel/src/App.tsx
@@ -25,12 +25,8 @@ function App() {
                 <InfoSectionRight imageSrc={InfoImageTwo} direction={"right"} header={"About Us"}
                                   text={"At Bullel, we're passionate about creating challenging and rewarding games that push the limits of gameplay and storytelling. Our team of dedicated developers works tirelessly to craft unique gaming experiences that are both beautiful and thrilling.\n" +
                                       "We're inspired by the mid-journey art style, and we believe that it adds an extra layer of depth to our games."}/>
-                <InfoSectionRight imageSrc={InfoImageOne} direction={"left"} header={"Idk yet"}
-                                  text={"Bullel to gra jest gra Bull el to gra jest gra Bu Bullel to gra jest gra " +
-                                      "Bullel to gra jest gBullel to gra jest gra ra " +
-                                      "Bullel to gBullel to gra jest gra ra jest gra " +
-                                      "Bullel to gra jest gra lBullel to gra jest gra Bullel to gra jest gra Bullel to gra jest gra " +
-                                      "Bullel to gra jest gra lel to gra jesBullel to gra jest gra t gra "}/>
+                <InfoSectionRight imageSrc={InfoImageOne} direction={"left"} header={"Midjourney"}
+                                  text={"At Bullel, we're always pushing the boundaries of what's possible in game development. That's why we chose to use Mid-Journey AI to generate our game assets. This powerful tool allows us to create stunning, high-quality visuals that immerse players in our dark fantasy world like never before. With Mid-Journey AI, we're able to generate detailed, lifelike environments and characters that truly bring our game to life."}/>
                 <div style={{height: "100px"}}></div>
             </Col>
             <Footer/>
