a074c6d082e9fd176da917567fd8f6e49316b44a
Wojciech-Mazurowski-digimuth <wojciech.mazurowski@digimuth.com>
2023-03-26 00:30:05 +0100

    robie

--- emakreting-bullel/src/App.tsx
+++ emakreting-bullel/src/App.tsx
@@ -12,33 +12,30 @@ import InfoImageTwo from "./assets/chrome_FI1j4IorZ2.png";
 
 
 import Footer from "./components/footer/footer"
+
 function App() {
-  return (
-    <Row className="App">
-        <PageHero/>
-        <Col xxl={{offset:2,span:20}} xl={{span:24, offset:0}}>
-            <AboutSection/>
-            <ReviewsSection/>
-            <FindUsSection/>
-            <Contact/>
-            <InfoSectionRight imageSrc={InfoImageTwo} direction={"right"} header={"About Us"} text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dignissim nulla leo, in feugiat\n" +
-                "                        felis vehicula elementum. Nulla facilisi. Suspendisse elementum, est eu posuere suscipit, eros\n" +
-                "                        quam\n" +
-                "                        iaculis elit, a aliquam lacus nibh at nulla. Nam mattis, dui quis hendrerit tempor, nisl felis\n" +
-                "                        venenatis sem, vehicula at turpis. Suspendisse dolor lacus, placerat eget\n" +
-                "                        rhoncus\n" +
-                "                        eget, aliquam non lectus. Aliquam vitae tincidunt turpis. Ut fermentum odio velit, ut\n" +
-                "                        sollicitudin"}/>
-            <InfoSectionRight imageSrc={InfoImageOne} direction={"left"} header={"Idk yet"} text={"Bullel to gra jest gra Bull el to gra jest gra Bu Bullel to gra jest gra " +
-                "Bullel to gra jest gBullel to gra jest gra ra " +
-                "Bullel to gBullel to gra jest gra ra jest gra " +
-                "Bullel to gra jest gra lBullel to gra jest gra Bullel to gra jest gra Bullel to gra jest gra " +
-                "Bullel to gra jest gra lel to gra jesBullel to gra jest gra t gra "}/>
-            <div style={{height:"100px"}}></div>
-        </Col>
-        <Footer/>
-    </Row>
-  );
+    return (
+        <Row className="App">
+            <PageHero/>
+            <Col xxl={{offset: 2, span: 20}} xl={{span: 24, offset: 0}}>
+                <AboutSection/>
+                <ReviewsSection/>
+                <FindUsSection/>
+                <Contact/>
+                <InfoSectionRight imageSrc={InfoImageTwo} direction={"right"} header={"About Us"}
+                                  text={"At Bullel, we're passionate about creating challenging and rewarding games that push the limits of gameplay and storytelling. Our team of dedicated developers works tirelessly to craft unique gaming experiences that are both beautiful and thrilling.\n" +
+                                      "We're inspired by the mid-journey art style, and we believe that it adds an extra layer of depth to our games."}/>
+                <InfoSectionRight imageSrc={InfoImageOne} direction={"left"} header={"Idk yet"}
+                                  text={"Bullel to gra jest gra Bull el to gra jest gra Bu Bullel to gra jest gra " +
+                                      "Bullel to gra jest gBullel to gra jest gra ra " +
+                                      "Bullel to gBullel to gra jest gra ra jest gra " +
+                                      "Bullel to gra jest gra lBullel to gra jest gra Bullel to gra jest gra Bullel to gra jest gra " +
+                                      "Bullel to gra jest gra lel to gra jesBullel to gra jest gra t gra "}/>
+                <div style={{height: "100px"}}></div>
+            </Col>
+            <Footer/>
+        </Row>
+    );
 }
 
 export default App;
