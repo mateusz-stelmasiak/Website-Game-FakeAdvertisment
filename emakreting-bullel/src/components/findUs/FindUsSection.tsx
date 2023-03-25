e2507657e6bfc8a82dcc1b07ccbc22235afd128f
Wojciech-Mazurowski-digimuth <wojciech.mazurowski@digimuth.com>
2023-03-25 20:43:42 +0100

    find us design update

--- emakreting-bullel/src/components/findUs/FindUsSection.tsx
+++ emakreting-bullel/src/components/findUs/FindUsSection.tsx
@@ -1,30 +1,30 @@
 import SteamLogo from "../../assets/Steam_icon_logo.png"
 import EpicLogo from "../../assets/epic-games-2.svg"
+import FaceLogo from "../../assets/Facebook_f_logo_(2019).png"
+import TwitterLogo from "../../assets/Logo blue.svg"
+import InstagramLogo from "../../assets/Instagram_logo_2016.png"
 import "./findUsSection.css"
 
 export default function FindUsSection() {
 
 
     return <>
-        <div className={"aboutHeader"}>
+        <div style={{marginBottom:"60px",marginTop:"80px",marginLeft:"60px"}} className={"aboutHeader"}>
             Find us
             <div className={"aboutHeaderDecoration"}>
                 Find us
             </div>
         </div>
-        <div className={"findUsSection"}>
+        <div className={"findUsSection"} style={{marginBottom:"80px"}}>
             <img className={"logo"} src={SteamLogo}/>
             <img className={"logo"} src={EpicLogo}/>
-            <img className={"logo"} src={SteamLogo}/>
-            <img className={"logo"} src={SteamLogo}/>
+            <img className={"logo"} src={InstagramLogo}/>
+        </div>
+        <div className={"findUsSection"}>
+            <img className={"logo"} src={FaceLogo}/>
+            <img className={"logo"} src={TwitterLogo}/>
         </div>
 
-        <div style={{marginTop:100,marginLeft:160}} className={"findUsSection"}>
-            <img className={"logo"} src={SteamLogo}/>
-            <img className={"logo"} src={SteamLogo}/>
-            <img className={"logo"} src={SteamLogo}/>
-
-        </div>
     </>
 
 }
--- emakreting-bullel/src/components/reviewsSection/reviewsSection.tsx
+++ emakreting-bullel/src/components/reviewsSection/reviewsSection.tsx
@@ -5,7 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ReviewCard from "../reviewCard/reviewCard";
 import SteamLogo from "../../assets/Steam_icon_logo.png"
-
+import EpicLogo from "../../assets/epic-games-2.svg"
 
 export default function ReviewsSection() {
     let settings = {
@@ -26,24 +26,39 @@ export default function ReviewsSection() {
         <Slider dots={true} infinite={true} speed={500} slidesToShow={4} slidesToScroll={1}>
             <div className={"reviewPlaceholder"}>
                 <ReviewCard
-                    reviewText={"\"Super gra, spędziłem w niej 3 godziny a nadal nie moge przejść drugiego poziomu\" - BlackSheep7"}
+                    reviewText={"\"This game is insane! Bullel's difficulty level is through the roof, but it's so rewarding when you make progress.\" - ProGamerX"}
                     companyName={"STEAM"} logo={SteamLogo} starCount={4.5}/>
 
             </div>
             <div className={"reviewPlaceholder"}>
-                <h3>2</h3>
+                <ReviewCard
+                    reviewText={"\"Bullel is incredibly challenging! I've been playing for hours and still can't pass the second level.\" - MasterBlaster"}
+                    companyName={"EPIC GAMES"} logo={EpicLogo} starCount={4.5}/>
+
             </div>
             <div className={"reviewPlaceholder"}>
-                <h3>3</h3>
+                <ReviewCard
+                    reviewText={"\"Bullel is the ultimate bullet hell game! It's not for the faint of heart, but if you're up for the challenge, it's so worth it.\" - BulletStormer"}
+                    companyName={"EPIC GAMES"} logo={EpicLogo} starCount={4.5}/>
+
             </div>
             <div className={"reviewPlaceholder"}>
-                <h3>4</h3>
+                <ReviewCard
+                    reviewText={"\"I can't get enough of Bullel! The fast-paced gameplay and intense difficulty make it the perfect game for adrenaline junkies.\" - BulletFury"}
+                    companyName={"STEAM"} logo={SteamLogo} starCount={4.5}/>
+
             </div>
             <div className={"reviewPlaceholder"}>
-                <h3>5</h3>
+                <ReviewCard
+                    reviewText={"\"Amazing game, cant stop playing this, fast-paced and for hardcore players only!!\" - BlackSheep7"}
+                    companyName={"STEAM"} logo={SteamLogo} starCount={4.5}/>
+
             </div>
             <div className={"reviewPlaceholder"}>
-                <h3>6</h3>
+                <ReviewCard
+                    reviewText={"\"Super gra, spędziłem w niej 3 godziny a nadal nie moge przejść drugiego poziomu\" - BlackSheep7"}
+                    companyName={"STEAM"} logo={SteamLogo} starCount={4.5}/>
+
             </div>
         </Slider>
     </div>
