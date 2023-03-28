612ea389239efd51e644c44356b95a86499dec23
mateusz-stelmasiak <46268673+mateusz-stelmasiak@users.noreply.github.com>
2023-03-28 21:52:47 +0200

    Timeline

--- emakreting-bullel/src/components/ourGamesSection/ourGamesSection.tsx
+++ emakreting-bullel/src/components/ourGamesSection/ourGamesSection.tsx
@@ -3,25 +3,29 @@ import {Radio, Timeline} from 'antd';
 import "./ourGameSection.css"
 import Test from "../../assets/asdf-removebg-preview.png"
 import Taiko from "../../assets/Taiko.png"
-
+import Bullel from "../../assets/bullel-cover.jpg"
 export default function OurGameSection() {
 
     let ourGames = [
         {
-            label: <div style={{fontSize:24,height:"100%"}}>Isaac game clone - 2021-12-28</div>,
+            label: <div style={{fontSize:24,height:"100%"}}>Company created [2020-11-18]</div>,
+            children: <div style={{height:'150px',width:'10px'}}></div> ,
+        },
+        {
+            label: <div style={{fontSize:24,height:"100%"}}>[2021-12-28] Isaac game clone</div>,
             children: <img src={Test}/> ,
         },
         {
-            label: <div style={{fontSize:24,height:"100%"}}>Taiko game clone - 2022-04-22</div>,
+            label: <div style={{fontSize:24,height:"100%"}}>Taiko game clone [2022-04-22]</div>,
             children: <img src={Taiko}/> ,
         },
         {
-            label: <div style={{fontSize:24,height:"100%"}}>Isaac game clone - 2021-12-28</div>,
-            children: <img src={Test}/> ,
+            label: <div style={{fontSize:24,height:"100%"}}>[2022-08-10] Bullel - eternal storm</div>,
+            children: <img src={Bullel}/> ,
         },
         {
-            label: <div style={{fontSize:24,height:"100%"}}>Isaac game clone - 2021-12-28</div>,
-            children: <img src={Test}/> ,
+            label: "",
+            children: <div style={{height:'30px',width:'10px'}}></div>  ,
         },
     ]
 
